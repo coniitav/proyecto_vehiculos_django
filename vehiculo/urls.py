ed0d269fd6216ea3fce9e9493215d379a58e5edf
coniitav <constanza.vergara.villar@gmail.com>
2024-12-19 00:09:45 -0300

    agregar usuarios con permisos y listado de vehiculos

--- vehiculo/urls.py
+++ vehiculo/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import index, add
+from .views import index, add, sign_up, sign_up_create, sign_in, sign_out, listar
 
 urlpatterns = [
     path("", index, name="index"),
     path("add", add, name="add"),
+    path("sign_up", sign_up, name="sign_up"),
+    path("sign_up/create", sign_up_create, name="sign_up_create"),
+    path("sign_in", sign_in, name="sign_in"),
+    path("sign_out", sign_out, name="sign_out"),
+    path("listar", listar, name="listar"),
 ]
--- vehiculo/views.py
+++ vehiculo/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
+from django.contrib import messages
 from .models import Vehiculo
 from datetime import date
 
@@ -24,3 +27,71 @@ def add(request):
         return redirect("/")
 
     return render(request, "vehiculo/add.html")
+
+def sign_up(request):
+    return render(request, "vehiculo/sign_up.html")
+
+def sign_up_create(request):
+    if request.method == "POST":
+        data = request.POST.copy()
+
+        nuevo_user = User.objects.filter(username = data["username"]).first()
+        if nuevo_user is not None:
+            messages.error(request, "Este email ya está registrado")
+            return redirect("/sign_up")
+        
+        nuevo_permiso = Permission.objects.filter(codename = "visualizar_catalogo").first()
+        if nuevo_permiso is None:
+            content_type = ContentType.objects.get_for_model(Vehiculo)
+            nuevo_permiso = Permission()
+            nuevo_permiso.codename = "visualizar_catalogo"
+            nuevo_permiso.name = "Puede visualizar el catálogo de los vehículos"
+            nuevo_permiso.content_type = content_type
+            nuevo_permiso.save()
+        
+        if nuevo_user is None:
+            nuevo_user = User()
+            nuevo_user.username = data["username"]
+            nuevo_user.first_name = data["nombres"]
+            nuevo_user.last_name = data["apellidos"]
+            nuevo_user.email = data["email"]
+            nuevo_user.is_active = True
+            nuevo_user.save()
+
+            nuevo_user.set_password(data["password"])
+            nuevo_user.user_permissions.add(nuevo_permiso)
+            nuevo_user.save()
+            messages.success(request, "Usuario creado con éxito")
+
+    return redirect("/sign_in")
+
+def sign_in(request):
+    from django.contrib.auth import authenticate, login
+    if request.method == "POST":
+        data = request.POST.copy()
+        username = data["username"]
+        password = data["password"]
+
+        usuario_valido = authenticate(request, username = username, password = password)
+
+        if usuario_valido is not None:
+            login(request, usuario_valido)
+            return redirect("/")
+        else:
+            messages.error(request, "Usuario o contraseña incorrecto")
+            pass
+
+    return render(request, "vehiculo/sign_in.html")
+
+def sign_out(request):
+    from django.contrib.auth import logout
+    logout(request)
+    messages.success(request, "Sesión cerrada")
+    return redirect("/")
+
+def listar(request):
+    vehiculos = Vehiculo.objects.all()
+    context = {
+        "vehiculos": vehiculos
+    }
+    return render(request, "vehiculo/listar.html", context)
